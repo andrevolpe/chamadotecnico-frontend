b0f151725bd39d071fb5998f6e810eabf4af7054
andrevolpe <andrevolpe@hotmail.com.br>
2020-06-15 13:31:51 +0000

    nova 3

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import red from '@material-ui/core/colors/green';
+import green from '@material-ui/core/colors/green';
 
 const theme = createMuiTheme({
   palette: {
