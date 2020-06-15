d02e3d117230d554ee9dfb7682097a905a348673
andrevolpe <andrevolpe@hotmail.com.br>
2020-06-15 04:52:56 +0000

    versao1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import red from '@material-ui/core/colors/red';
+// import amber from '@material-ui/core/colors/amber';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+  palette: {
+    primary: red,
+    // secondary: amber,
+  },  
+});
+
+ReactDOM.render(
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>
+, document.getElementById('root'));
